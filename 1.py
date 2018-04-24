55db3dea3cf1c41182402bd1e4e4b4fef86fef30
YIPG <around.mr.child@gmail.com>
2018-04-24 20:53:12 +0900

    1章終わり

--- 1.py
+++ 1.py
@@ -1,4 +1,9 @@
+# NLP100本ノック! かっとばせ〜〜！
+
 # 1章
+
+import re
+import random
 
 # 1
 
@@ -28,8 +33,7 @@ def p3():
 # 4
 
 
-def p4():
-    s = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+def p4(s):
     vocab = ""
     vocab_l = []
     for j, i in enumerate(s):
@@ -41,6 +45,8 @@ def p4():
         else:
             vocab += i
     return vocab_l
+
+# 5
 
 
 def p5():
@@ -66,6 +72,8 @@ def p5():
 
     return vocab_dic
 
+# 6
+
 
 def p6(s, scale):
     vocab = ""
@@ -83,7 +91,7 @@ def p6(s, scale):
             vocab_l.append(vocab)
             vocab = ""
         elif (j == len(s) - 1):
-            if i == "," or i == "." or i ==" ":
+            if i == "," or i == "." or i == " ":
                 vocab_l.append(vocab)
             else:
                 vocab += i
@@ -93,18 +101,111 @@ def p6(s, scale):
         else:
             vocab += i
 
-
     for i in range(len(vocab_l) - scale + 1):
         v_bigram.append(vocab_l[i:i + scale])
 
     return s_bigram, v_bigram
 
+# 7
+
+
+def p7():
+    X = p6("paraparaparadise", 2)[0]
+    Y = p6("paragraph", 2)[0]
+
+    # 集合型に変換する
+    X, Y = set(X), set(Y)
+
+    return "和集合は" + str(X | Y), "積集合は" + str(X & Y), "差集合は" + str(X ^ Y), "Xは" + str("se" in X) + "です", "Yは" + str("se" in Y) + "です"
+
+# 8
+
+
+def p8(x, y, z):
+    return "{}時の{}は{}".format(x, y, z)
+
+# 9
+
+
+def p9_encrypt(s):
+    # 文字コードはUnicode想定.
+    output = ""
+    for i in s:
+        if 97 <= ord(i) <= 122:
+            output += str(219) + str(ord(i))
+        else:
+            output += i
+    return output
+
+# 9
+
+
+def p9_decrypt(s):
+    # 正規表現を使う
+    encrypt_list = re.findall("219[9][789]|219[1][012]\d", s)
+    for i in encrypt_list:
+        s = re.sub(i, chr(int(i[3:])), s)
+    return s
+
+# 10
+
+
+def p10(s):
+    vocab_l = p4(s)
+    for i, vocab in enumerate(vocab_l):
+        if len(vocab) < 5:
+            continue
+        else:
+            vocab_l[i] = vocab.replace(
+                vocab[1:-1], "".join(random.sample(vocab[1:-1], len(vocab) - 2)))
+    return " ".join(vocab_l)
 
 
 if __name__ == "__main__":
     print(p1())
     print(p2())
     print(p3())
-    print(p4())
+    print(p4("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."))
     print(p5())
     print(p6("I am an NLPer", 2))
+    print(p7())
+    print(p8(12, "気温", 22.4))
+    print(p9_encrypt("I am GOD FATHER desu"))
+    print(p9_decrypt("I 21997219109 GOD FATHER 219100219101219115219117"))
+    print(p10("I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."))
+
+# 出力例
+
+# desserts
+# パトカー
+# パタトクカシーー
+# ['Now', 'I', 'need', 'a', 'drink', 'alcoholic', 'of', 'course', 'after', 'the', 'heavy', 'lectures', 'involving', 'quantum', 'mechanics']
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# 11
+# 12
+# 13
+# 14
+# 15
+# 16
+# 17
+# 18
+# {'Hi': 0, 'H': 1, 'Li': 2, 'Be': 3, 'Bo': 4, 'C': 16, 'N': 9, 'O': 7, 'F': 8, 'Na': 10, 'Mi': 11, 'Al': 12, 'Si': 13, 'Pe': 14, 'S': 15, 'Ar': 17, 'Ki': 18}
+# IamanNLPer
+# (['Ia', 'am', 'ma', 'an', 'nN', 'NL', 'LP', 'Pe', 'er'], [['I', 'am'], ['am', 'an'], ['an', 'NLPer']])
+# paraparaparadise
+# paragraph
+# ("和集合は{'se', 'pa', 'ar', 'di', 'gr', 'ag', 'is', 'ad', 'ph', 'ap', 'ra'}", "積集合は{'ra', 'ar', 'ap', 'pa'}", "差集合は{'se', 'di', 'ag', 'is', 'ad', 'gr', 'ph'}", 'XはTrueです', 'YはFalseです')
+# 12時の気温は22.4
+# I 21997219109 GOD FATHER 219100219101219115219117
+# I am GOD FATHER desu
+# I c'uondlt bvieele that I cloud aallucty unstnedard what I was radieng : the peamneonhl power of the haumn mind
